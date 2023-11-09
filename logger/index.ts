dc2c07da870b8aabdb303b0f6e0849c41372e57d
luya <1456887752@qq.com>
2023-11-09 18:04:10 +0800

    update

--- logger/index.ts
+++ logger/index.ts
@@ -1,2 +1,12 @@
-import Logger from "https://deno.land/x/logger@v1.1.2/logger.ts";
-export const logger = new Logger();
+import moment from "https://deno.land/x/momentjs@2.29.1-deno/mod.ts";
+export default {
+  info: function(...args: any[]) {
+    return console.info(moment(Date.now()).format('YY/MM/DD HH:mm:ss'),...args);
+  },
+  error: function(...args: any[]) {
+    return console.error(moment(Date.now()).format('YY/MM/DD HH:mm:ss'),...args);
+  },
+  debug: function(...args: any[]) {
+    return console.debug(moment(Date.now()).format('YY/MM/DD HH:mm:ss'),...args);
+  },
+};
